950f68f9d530a0642441ca2fe80e82d855ac27b9
DanyaKrov <74681279+DanyaKrov@users.noreply.github.com>
2021-12-16 20:58:56 +0300

    Update itc_rev_par_list.cpp

--- itc_rev_par_list.cpp
+++ itc_rev_par_list.cpp
@@ -2,11 +2,11 @@
 
 void itc_rev_par_list(vector <int> &mass){
     if (mass.size() != 0){
-    int x;
-    for (int i = 1; i < mass.size(); i += 2) {
+        int x;
+        for (int i = 1; i < mass.size(); i += 2) {
             x = mass[i - 1];
             mass[i - 1] = mass[i];
             mass[i] = x;
-    }
+        }
     }
 }
