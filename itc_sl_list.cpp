eacb647db8d75776530b817c7bf4bcbf1357aaff
DanyaKrov <74681279+DanyaKrov@users.noreply.github.com>
2021-12-16 20:58:07 +0300

    Update itc_sl_list.cpp

--- itc_sl_list.cpp
+++ itc_sl_list.cpp
@@ -2,11 +2,11 @@
 
 int itc_sl_list(const vector <int> &mass){
     if (mass.size() != 0){
-    int count = 0;
-    for (int i = 1; i < mass.size(); i++){
-        if (mass[i] > mass[i - 1])
-            count++;
-    }
+        int count = 0;
+        for (int i = 1; i < mass.size(); i++){
+            if (mass[i] > mass[i - 1])
+                count++;
+        }
     return count;
     }
     else
