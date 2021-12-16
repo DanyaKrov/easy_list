df614fdde432c47a80e0c5ae57431bb16f5746e8
DanyaKrov <74681279+DanyaKrov@users.noreply.github.com>
2021-12-16 20:43:04 +0300

    Update itc_super_shift_list.cpp

--- itc_super_shift_list.cpp
+++ itc_super_shift_list.cpp
@@ -2,20 +2,20 @@
 
 void itc_super_shift_list(vector <int> &mass, int n){
     if (mass.size() != 0){
-            if (n < 0){
-        itc_rev_list(mass);
-        n *= -1;
-        while (n != 0){
-            itc_rshift_list(mass);
-            n--;
+        if (n < 0){
+            itc_rev_list(mass);
+            n *= -1;
+            while (n != 0){
+                itc_rshift_list(mass);
+                n--;
+            }
+            itc_rev_list(mass);
         }
-        itc_rev_list(mass);
-    }
-    else{
-        while (n != 0){
-            itc_rshift_list(mass);
-            n--;
+        else{
+            while (n != 0){
+                itc_rshift_list(mass);
+                n--;
+            }
         }
-    }
     }
 }
