5621f029faae8f29187f6843965eb5efa1381047
DanyaKrov <74681279+DanyaKrov@users.noreply.github.com>
2021-12-16 20:53:03 +0300

    Update itc_positive_list.cpp

--- itc_positive_list.cpp
+++ itc_positive_list.cpp
@@ -2,12 +2,12 @@
 
 int itc_positive_list(const vector <int> &mass){
     if (mass.size() != 0){
-    int count = 0;
-    for (int i = 0; i < mass.size(); i++){
-        if (mass[i] > 0)
-            count++;
-    }
-    return count;
+        int count = 0;
+        for (int i = 0; i < mass.size(); i++){
+            if (mass[i] > 0)
+                count++;
+        }
+        return count;
     }
     else
         return 0;
