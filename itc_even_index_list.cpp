616f1266300ed99b3f7bdb265d376158210515bd
DanyaKrov <74681279+DanyaKrov@users.noreply.github.com>
2021-12-16 20:50:55 +0300

    Update itc_even_index_list.cpp

--- itc_even_index_list.cpp
+++ itc_even_index_list.cpp
@@ -5,6 +5,6 @@ void itc_even_index_list (const vector <int> &mass, vector <int> &mass2){
         for (int i = 0; i < mass.size(); i++){
             if (i % 2 == 0)
                 mass2.push_back(mass[i]);
-    }
+        }
     }
 }
