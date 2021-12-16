519428dc6a0f56b9c25b82c2dcea5bfb4ef43e56
DanyaKrov <74681279+DanyaKrov@users.noreply.github.com>
2021-12-16 20:46:52 +0300

    Delete list_print.cpp

--- list_print.cpp
+++
@@ -1,9 +0,0 @@
-#include "easy_list.h"
-
-void list_print(const vector <int> mass){
-    if (mass.size() != 0){
-        for (int i = 0; i < mass.size(); i++){
-            cout <<mass[i];
-        }
-    }
-}
