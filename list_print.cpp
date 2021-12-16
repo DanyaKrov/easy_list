0fb2e075adb00723677c5f711beebc8296fedced
DanyaKrov <74681279+DanyaKrov@users.noreply.github.com>
2021-12-16 20:40:31 +0300

    Update list_print.cpp

--- list_print.cpp
+++ list_print.cpp
@@ -1,9 +1,9 @@
 #include "easy_list.h"
 
-void list_print (const vector <int> mass){
+void list_print(const vector <int> mass){
     if (mass.size() != 0){
         for (int i = 0; i < mass.size(); i++){
             cout <<mass[i];
-    }
+        }
     }
 }
