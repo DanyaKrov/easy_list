e21b6bb5a5d5c86f9bd52e67b77db241e514d0cb
DanyaKrov <74681279+DanyaKrov@users.noreply.github.com>
2021-12-16 20:55:36 +0300

    Update itc_even_index_list.cpp

--- itc_even_index_list.cpp
+++ itc_even_index_list.cpp
@@ -1,7 +1,7 @@
 #include "easy_list.h"
 
 void itc_even_index_list (const vector <int> &mass, vector <int> &mass2){
-    if (mass.size() != 0 && mass2.size() != 0){
+    if (mass.size() != 0){
         for (int i = 0; i < mass.size(); i++){
             if (i % 2 == 0)
                 mass2.push_back(mass[i]);
