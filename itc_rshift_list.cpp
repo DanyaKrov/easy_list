f8815513fba5489e07c386fb4bc81ae4aa2df837
DanyaKrov <74681279+DanyaKrov@users.noreply.github.com>
2021-12-16 21:15:27 +0300

    Update itc_rshift_list.cpp

--- itc_rshift_list.cpp
+++ itc_rshift_list.cpp
@@ -2,10 +2,10 @@
 
 void itc_rshift_list(vector <int> &mass){
     if (mass.size() != 0){
-        for (int i = 0; i < mass.size() - 1; i++) {
-            mass[i + 1] = mass[i];
-        }
         int x = mass[mass.size() - 1];
+        for (int i = mass.size() - 1; i > 0; i--) {
+            mass[i] = mass[i - 1];
+        }
         mass[0] = x;
     }
 }
